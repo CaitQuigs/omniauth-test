512298f276ead1ce2142621fb55d6afe32b17cd1
Caitlin Quigley <cquigley13@gmail.com>
2020-04-16 18:05:22 -0400

    Update to Rails v6.0.2.2

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   get '/auth/:provider/callback' => 'sessions#create'
 
-  get '/signout' => 'sessions#destroy', :as => :signout
+  post '/signout' => 'sessions#destroy', as: :signout
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
